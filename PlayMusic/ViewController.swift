6b8bf1e06c9a6bfd0d87334a7c6ef0e012c01770
ngoctan95 <nguyenngoctan44@gmail.com>
2018-01-22 02:27:02 +0700

    Playmusic

--- PlayMusic/ViewController.swift
+++ PlayMusic/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
+var audio = AVAudioPlayer()
+    var count : Int!
+    
+    @IBAction func btnPlay(_ sender: Any) {
+        audio.play()
+        FEPlay.setImage(UIImage(named : "pause"), for: .normal)
+        count = count + 1
+        if (count % 2 == 0){
+            audio.stop()
+            print("stop")
+        }else{
+            print("playing")
+            audio.play()
+        }
+    }
 
+    @IBOutlet weak var FEPlay: UIButton!
+    @IBAction func slider_change(_ sender: UISlider) {
+        print(sender.value)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       count = 1
+        audio = try! AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: Bundle.main.path(forResource: "music", ofType: ".mp3")!) as URL)
+//        audio = try! AVAudioPlayer(contentsOf: URL.init(from: Bundle.main.path(forResource: "music", ofType: "mp3") as! Decoder))
+        audio.prepareToPlay()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
