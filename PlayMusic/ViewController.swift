81096890c2c7ad2c19568c5f69314008f60796ef
ngoctan95 <nguyenngoctan44@gmail.com>
2018-01-22 21:13:30 +0700

    change

--- PlayMusic/ViewController.swift
+++ PlayMusic/ViewController.swift
@@ -8,33 +8,55 @@
 
 import UIKit
 import AVFoundation
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 var audio = AVAudioPlayer()
     var count : Int!
     
     @IBAction func btnPlay(_ sender: Any) {
         audio.play()
-        FEPlay.setImage(UIImage(named : "pause"), for: .normal)
-        count = count + 1
+        
+        
         if (count % 2 == 0){
-            audio.stop()
+            FEPlay.setImage(UIImage(named : "play.png"), for: .normal)
+            audio.pause()
             print("stop")
         }else{
+            FEPlay.setImage(UIImage(named : "pause.png"), for: .normal)
+            audio.play(atTime: 0)
             print("playing")
             audio.play()
         }
+        count = count + 1
     }
 
+    @IBOutlet weak var sliderDist: UISlider!
+    @IBOutlet weak var lblTo: UILabel!
+    @IBOutlet weak var lblFrom: UILabel!
     @IBOutlet weak var FEPlay: UIButton!
     @IBAction func slider_change(_ sender: UISlider) {
-        print(sender.value)
+        audio.volume = sender.value
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.FEPlay.setImage(UIImage(named: "play.png"), for: .normal)
        count = 1
         audio = try! AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: Bundle.main.path(forResource: "music", ofType: ".mp3")!) as URL)
-//        audio = try! AVAudioPlayer(contentsOf: URL.init(from: Bundle.main.path(forResource: "music", ofType: "mp3") as! Decoder))
         audio.prepareToPlay()
+        audio.play()
+        lblTo.text = String(format: "%2.2f",audio.duration/60)
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(getTrackSlider), userInfo: nil, repeats: true)
     }
+    @objc func getTrackSlider(){
+        //print(audio.currentTime)
+//        lblFrom.text = String("\(audio.currentTime / 60) : \(audio.currentTime.truncatingRemainder(dividingBy: 60))")
+//
+        lblFrom.text = String(format: "%2.2f", audio.currentTime/60)
+        sliderDist.value = Float(audio.currentTime / audio.duration)
+    }
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Done")
+    }
+
 }
 
